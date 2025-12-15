d5b692197187a389b23da5fc45c06db63e3e1673
JesusMtnez <jesusmartinez93@gmail.com>
2025-12-15 15:59:17 +0100

    kubernetes-sigs/nfs-subdir-external-provisioner: init at 4.0.18

---
+++ charts/kubernetes-sigs/nfs-subdir-external-provisioner/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://kubernetes-sigs.github.io/nfs-subdir-external-provisioner/";
+  chart = "nfs-subdir-external-provisioner";
+  version = "4.0.18";
+  chartHash = "sha256-STkDh6TzNnouJvHYmwmm42dSN7vDfguxhOz01aOa3Dc=";
+}
