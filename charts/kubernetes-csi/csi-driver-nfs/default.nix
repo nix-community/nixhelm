048cf7c14caeba8e9876bac685a293b6498b7b49
apetrovic <petrovicante6@gmail.com>
2025-12-30 15:46:17 +0100

    set version to latest

--- charts/kubernetes-csi/csi-driver-nfs/default.nix
+++ charts/kubernetes-csi/csi-driver-nfs/default.nix
@@ -1,6 +1,6 @@
 {
   repo = "https://github.com/kubernetes-csi/csi-driver-nfs/charts";
   chart = "csi-driver-nfs";
-  version = "0.0.0";
+  version = "latest";
   chartHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
 }
