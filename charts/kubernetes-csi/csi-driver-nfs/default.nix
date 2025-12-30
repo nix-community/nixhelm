8842a12719af9e6478a23923d1e29cfcac8243f7
apetrovic <petrovicante6@gmail.com>
2025-12-30 15:37:30 +0100

    kubernetes-csi/csi-driver-nfs

---
+++ charts/kubernetes-csi/csi-driver-nfs/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://github.com/kubernetes-csi/csi-driver-nfs/charts";
+  chart = "csi-driver-nfs";
+  version = "0.0.0";
+  chartHash = "sha256-AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA=";
+}
