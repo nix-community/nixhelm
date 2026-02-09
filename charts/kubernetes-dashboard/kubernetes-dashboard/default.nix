10dc9a0dea3aeb8747c599ba8384a32caee458ed
Damon River <damonriver@fastmail.com>
2026-02-09 12:26:19 -0600

    Update chart repo in kubernetes-dashboard

--- charts/kubernetes-dashboard/kubernetes-dashboard/default.nix
+++ charts/kubernetes-dashboard/kubernetes-dashboard/default.nix
@@ -1,5 +1,5 @@
 {
-  repo = "https://kubernetes.github.io/dashboard/";
+  repo = "https://kubernetes-retired.github.io/dashboard/";
   chart = "kubernetes-dashboard";
   version = "7.14.0";
   chartHash = "sha256-n0HvDe1+pS9Zu4JqP9PwWkubAvp9F8KGaB0tPaLShHA=";
