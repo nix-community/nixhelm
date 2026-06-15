5a9a25f880ede29972dde5bcca22f5d84bbb1acf
Alexis Williams <alexis@typedr.at>
2026-06-15 11:33:34 -0700

    cloudflare/origin-ca-issuer: init at 0.6.9

---
+++ charts/cloudflare/origin-ca-issuer/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "oci://ghcr.io/cloudflare/origin-ca-issuer-charts";
+  chart = "origin-ca-issuer";
+  version = "0.6.9";
+  chartHash = "sha256-RwNZHbgB+Cwz22CDv5Vzneh4mzKL0HMqQsIqffa/52k=";
+}
