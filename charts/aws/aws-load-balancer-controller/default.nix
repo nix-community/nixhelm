9ff88cff7d62c24646a660f80e81770dccb456da
Alexis Williams <alexis@typedr.at>
2026-06-14 22:33:23 -0700

    aws/aws-load-balancer-controller: init at 3.4.0

---
+++ charts/aws/aws-load-balancer-controller/default.nix
@@ -0,0 +1,6 @@
+{
+  repo = "https://aws.github.io/eks-charts";
+  chart = "aws-load-balancer-controller";
+  version = "3.4.0";
+  chartHash = "sha256-buV3J7h/J4gKWwFfolE8zbe8eMLWpbPigcR2USc8gAQ=";
+}
