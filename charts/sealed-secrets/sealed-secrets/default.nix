6d593a269df385b72af918ace497ea1d76ec3443
Damon River <damonriver@fastmail.com>
2026-06-22 08:44:40 -0500

    Update sealed-secrets/sealed-secrets repo url

--- charts/sealed-secrets/sealed-secrets/default.nix
+++ charts/sealed-secrets/sealed-secrets/default.nix
@@ -1,5 +1,5 @@
 {
-  repo = "https://bitnami-labs.github.io/sealed-secrets/";
+  repo = "https://bitnami.github.io/sealed-secrets/";
   chart = "sealed-secrets";
   version = "2.18.6";
   chartHash = "sha256-UU9mdf65anGaqkCv31oLRFVbfKxoX/un29PAgfAJS8k=";
